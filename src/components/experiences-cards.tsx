19e4308dbb9facce8d8dfc2c08b58979016afce3
rafael mafort <rafaelmafortc@gmail.com>
2025-05-09 16:30:05 -0300

    feat: experience

--- src/components/experiences-cards.tsx
+++ src/components/experiences-cards.tsx
@@ -10,25 +10,28 @@ export function ExperiencesCards() {
     const t = useTranslations('experiences');
 
     return (
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-6">
             {Object.entries(experiences).map(([key, name]) => (
-                <div key={key} className="flex items-center gap-4">
-                    <Image
-                        src={`/experiences/${key}.svg`}
-                        alt={name}
-                        width={48}
-                        height={48}
-                        style={{
-                            height: 'auto',
-                            userSelect: 'none',
-                        }}
-                    />
-                    <div className="space-y-1">
-                        <p className="font-medium leading-none">{name}</p>
-                        <p className="text-sm text-muted-foreground">
-                            {t(`${key}_date`)}
-                        </p>
+                <div key={key} className="flex flex-col gap-1">
+                    <div className="flex items-center gap-4">
+                        <Image
+                            src={`/experiences/${key}.svg`}
+                            alt={name}
+                            width={48}
+                            height={48}
+                            style={{
+                                height: 'auto',
+                                userSelect: 'none',
+                            }}
+                        />
+                        <div className="space-y-1">
+                            <p className="font-medium leading-none">{name}</p>
+                            <p className="text-sm text-muted-foreground">
+                                {t(`${key}_date`)}
+                            </p>
+                        </div>
                     </div>
+                    <p className="text-base">{t(`${key}_description`)}</p>
                 </div>
             ))}
         </div>
